0907a94e07dbc18f7e59ee49a442f82c75896c3f
Oleksandr Kalita <olexadr.kalita@gmail.com>
2024-05-06 23:12:12 +0200

    .

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import {Head, Html, Main, NextScript} from "next/document"
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <meta charset="UTF-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            </Head>
+            
             <body>
                 <Main />
                 <NextScript />
